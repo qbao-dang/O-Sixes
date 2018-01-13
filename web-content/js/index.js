edc63414b6bde8f60c1305ca59709df8a128f35c
qbao-dang <32182378+qbao-dang@users.noreply.github.com>
2018-01-13 11:47:53 -0500

    added properties file reader

--- web-content/js/index.js
+++ web-content/js/index.js
@@ -1,5 +1,9 @@
-
-
+/*Module depenacies*/
+var PropertiesReader = require('../../node_modules/properties-reader');
+var properties = PropertiesReader('./resources/config.properties');
+var apiKey = properties.get(apiKey);
+const BLIZZARD_API_KEY = require('./resources/config.properties').resources
+const blizzard = require('../../node_modules/blizzard.js').initialize({appkey: });
 var http = require('http');
 var fs = require('fs');
 
