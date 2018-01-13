bd748c87630d9723e694a19937db597969bb19fd
qbao-dang <32182378+qbao-dang@users.noreply.github.com>
2018-01-13 15:33:45 -0500

    add passport-bnet

--- web-content/OAuth/config/blizzard.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const blizzard = require('blizzard.js').initialize({
-  apikey: "bh4942wy9b5k6cbmfmmjms6tez8chrvy"
-});
-
-module.exports = blizzard;
--- web-content/OAuth/test/test.js
+++
@@ -1,8 +0,0 @@
-/* global describe */
-'use strict';
-
-const lint = require('mocha-eslint');
-
-describe('Code Standards', function () {
-  lint(['server.js', 'config/**/*.js', 'routes/**/*.js', 'test/**/*.js']);
-});
