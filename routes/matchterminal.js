3427b4bcbd8310a2ce941895600d520389acae56
Viet (Macbook) <qv.lethe@gmail.com>
2018-01-21 08:37:35 -0500

    add comment and remove comments

--- routes/matchterminal.js
+++ routes/matchterminal.js
@@ -3,18 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  //res.status(200);
-/*
-  res.set({
-    'Content-Type': 'text/html',
-    'Cache-Control': 'no-cache',
-    'Connection': 'keep-alive'
-  });
-*/
   // Send data to the client
   res.render('matchterminal');
 });
-
-
 
 module.exports = router;
